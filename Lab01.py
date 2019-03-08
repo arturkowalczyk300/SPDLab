e240afb7ddc06c1118c66f7f2d6526cf205adf69
Artur <aaaa>
2019-03-08 15:43:31 +0100

    automatyczne sprawdzanie czasu trwania kazdej permutacji

--- Lab01.py
+++ Lab01.py
@@ -28,29 +28,39 @@ liczba_maszyn = 2
 liczba_zadan = 5
 czas = 0
 
+permutacje = []
+
 # wyswietla wszystkie permutacje
-# for p in permutacja(zadania):
-#    print(p)
+for p in permutacja(zadania):
+    permutacje.append(p)
+    #   print(p)
 
+# print(permutacje)
 # naturalna kolejnosc (1,2,3,4,5)
-kolejnosc = [0, 1, 2, 3, 4]
-# kolejnosc = calakolejnosc[1:5]
+naturalnakolejnosc = [0, 1, 2, 3, 4]
+kolejnosc = [0, 0, 0, 0, 0]
+for p in permutacje:
+    kolejnosc = p
+    for k in range(0, 5):
+        kolejnosc[k] = kolejnosc[k] - 1  # zmiana indeksowania na zgodne z tablicami (numeracja od zera)
 
-zakonczenie_zadan_1[kolejnosc[0]] = czas_na_maszynie_1[kolejnosc[0]]
-for i in range(1, 5):
-    print("i=", i)
-    zakonczenie_zadan_1[kolejnosc[i]] = zakonczenie_zadan_1[kolejnosc[i - 1]] + czas_na_maszynie_1[kolejnosc[i]]
+    zakonczenie_zadan_1[kolejnosc[0]] = czas_na_maszynie_1[kolejnosc[0]]
+    for i in range(1, 5):
+        # print("i=", i)
+        zakonczenie_zadan_1[kolejnosc[i]] = zakonczenie_zadan_1[kolejnosc[i - 1]] + czas_na_maszynie_1[kolejnosc[i]]
 
-zakonczenie_zadan_2[kolejnosc[0]] = zakonczenie_zadan_1[kolejnosc[0]] + czas_na_maszynie_2[kolejnosc[0]]
-for i in range(1, 5):
-    # jesli zadanie i sie zakonczylo na maszynie pierwszej to odpalam je na drugiej. jesli nie, czekam do jego konca.
-    if (zakonczenie_zadan_1[kolejnosc[i]] < zakonczenie_zadan_2[
-        kolejnosc[i - 1]]):  # jesli zakonczenie zadania nastapilo wczesniej
-        zakonczenie_zadan_2[kolejnosc[i]] = zakonczenie_zadan_2[kolejnosc[i - 1]] + czas_na_maszynie_2[kolejnosc[i]]
-    else:
-        zakonczenie_zadan_2[kolejnosc[i]] = zakonczenie_zadan_1[kolejnosc[i]] + czas_na_maszynie_2[kolejnosc[i]]
-cmax = zakonczenie_zadan_2[kolejnosc[4]]
-print("cmax=", cmax)
+    zakonczenie_zadan_2[kolejnosc[0]] = zakonczenie_zadan_1[kolejnosc[0]] + czas_na_maszynie_2[kolejnosc[0]]
+    for i in range(1, 5):
+        # jesli zadanie i sie zakonczylo na maszynie pierwszej to odpalam je na drugiej. jesli nie, czekam do jego konca.
+        if (zakonczenie_zadan_1[kolejnosc[i]] < zakonczenie_zadan_2[
+            kolejnosc[i - 1]]):  # jesli zakonczenie zadania nastapilo wczesniej
+            zakonczenie_zadan_2[kolejnosc[i]] = zakonczenie_zadan_2[kolejnosc[i - 1]] + czas_na_maszynie_2[kolejnosc[i]]
+        else:
+            zakonczenie_zadan_2[kolejnosc[i]] = zakonczenie_zadan_1[kolejnosc[i]] + czas_na_maszynie_2[kolejnosc[i]]
+    cmax = zakonczenie_zadan_2[kolejnosc[4]]
+    for k in range(0, 5):
+        kolejnosc[k] = kolejnosc[k] + 1  # zmiana indeksowania na naturalne z powrotem
+    print("kolejnosc=", p, "cmax=", cmax)
 
-print("czasy zakonczenia na maszynie 1", zakonczenie_zadan_1)
-print("czasy zakonczenia na maszynie 2", zakonczenie_zadan_2)
+# print("czasy zakonczenia na maszynie 1", zakonczenie_zadan_1)
+# print("czasy zakonczenia na maszynie 2", zakonczenie_zadan_2)
