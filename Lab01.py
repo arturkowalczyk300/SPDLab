da987264796e83c58cd30df9d32c73cccc445b7f
Artur <aaaa>
2019-03-10 10:14:49 +0100

    wizualizacja dwoch maszyn z trzech dla alg. Johnsona 3 maszynowego

--- Lab01.py
+++ Lab01.py
@@ -2,33 +2,32 @@
 # todo(3) - wizualizacje dla 3 maszyn
 # todo (6) - johnson dla 2 maszyn w osobnej funkcji
 # todo(7) - liczenie cmax dla 3 maszyn
+# todo (8) - naprawa sztywnej kolejnosci w wizualizacji
 import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-#konfiguracja dla 2 maszyn
+# konfiguracja dla 2 maszyn
 dwie_zadania = [1, 2, 3, 4, 5]
 dwie_czas_na_maszynie_1 = [4, 4, 10, 6, 2]
 dwie_czas_na_maszynie_2 = [5, 1, 4, 10, 3]
 dwie_liczba_maszyn = 2
 
+# konfiguracja dla 3 maszyn
+# trzy_zadania = [1, 2, 3, 4]
+# trzy_czas_na_maszynie_1 = [5, 4, 4, 3]
+# trzy_czas_na_maszynie_2 = [5, 5, 4, 5]
+# trzy_czas_na_maszynie_3 = [3, 2, 5, 7]
+# trzy_liczba_maszyn = 3
 
-#konfiguracja dla 3 maszyn
+# instancja t000
 trzy_zadania = [1, 2, 3, 4]
-trzy_czas_na_maszynie_1 = [5, 4, 4, 3]
-trzy_czas_na_maszynie_2 = [5, 5, 4, 5]
-trzy_czas_na_maszynie_3 = [3, 2, 5, 7]
+trzy_czas_na_maszynie_1 = [1, 9, 7, 4]
+trzy_czas_na_maszynie_2 = [3, 3, 8, 8]
+trzy_czas_na_maszynie_3 = [8, 5, 7, 6]
 trzy_liczba_maszyn = 3
 
-#instancja t000
-#trzy_zadania = [1, 2, 3, 4]
-#trzy_czas_na_maszynie_1 = [1, 9, 7, 4]
-#trzy_czas_na_maszynie_2 = [3, 3, 8,8]
-#trzy_czas_na_maszynie_3 = [8,5,7,6]
-#trzy_liczba_maszyn = 3
-
-
-#sekcja z danymi
+# sekcja z danymi
 kolory = ["red", "green", "blue", "cyan", "magenta"]
 zakonczenie_zadan_1 = [0, 0, 0, 0, 0]
 zakonczenie_zadan_2 = [0, 0, 0, 0, 0]
@@ -89,7 +88,7 @@ def wizualizacjaDwochMaszyn(arg_czas_na_maszynie_2, arg_kolejnosc,
 
 
 def wizualizacjaTrzechMaszyn(arg_czas_na_maszynie_2, arg_kolejnosc,
-                            arg_nazwa_pliku, arg_cmax):
+                             arg_nazwa_pliku, arg_cmax):
     plt.figure(figsize=(20, 7))
     # wizualizacja maszyny pierwszej
     plt.hlines(-1, 0, zakonczenie_zadan_1[arg_kolejnosc[0] - 1], colors=kolory[0], lw=4)
@@ -102,9 +101,7 @@ def wizualizacjaTrzechMaszyn(arg_czas_na_maszynie_2, arg_kolejnosc,
     plt.hlines(-1, zakonczenie_zadan_1[arg_kolejnosc[2] - 1], zakonczenie_zadan_1[arg_kolejnosc[3] - 1],
                colors=kolory[3],
                lw=4)
-    plt.hlines(-1, zakonczenie_zadan_1[arg_kolejnosc[3] - 1], zakonczenie_zadan_1[arg_kolejnosc[4] - 1],
-               colors=kolory[4],
-               lw=4)
+
     # wizualizacja maszyny drugiej
     plt.hlines(-2, zakonczenie_zadan_2[arg_kolejnosc[0] - 1] - arg_czas_na_maszynie_2[arg_kolejnosc[0] - 1],
                zakonczenie_zadan_2[arg_kolejnosc[0] - 1], colors=kolory[0], lw=4)
@@ -114,8 +111,6 @@ def wizualizacjaTrzechMaszyn(arg_czas_na_maszynie_2, arg_kolejnosc,
                zakonczenie_zadan_2[arg_kolejnosc[2] - 1], colors=kolory[2], lw=4)
     plt.hlines(-2, zakonczenie_zadan_2[arg_kolejnosc[3] - 1] - arg_czas_na_maszynie_2[arg_kolejnosc[3] - 1],
                zakonczenie_zadan_2[arg_kolejnosc[3] - 1], colors=kolory[3], lw=4)
-    plt.hlines(-2, zakonczenie_zadan_2[arg_kolejnosc[4] - 1] - arg_czas_na_maszynie_2[arg_kolejnosc[4] - 1],
-               zakonczenie_zadan_2[arg_kolejnosc[4] - 1], colors=kolory[4], lw=4)
     plt.margins(0.1)
     plt.grid()
     plt.xticks(np.arange(0, 40, 1))
@@ -126,7 +121,7 @@ def wizualizacjaTrzechMaszyn(arg_czas_na_maszynie_2, arg_kolejnosc,
     plt.close()
 
 
-def przegladKolejnosci(n, arg_kolejnosc, arg_czas_na_maszynie_1, arg_czas_na_maszynie_2):
+def przegladKolejnosci(n, arg_kolejnosc, arg_czas_na_maszynie_1, arg_czas_na_maszynie_2):  # n zadan
     for k in range(0, n):
         arg_kolejnosc[k] = arg_kolejnosc[k] - 1  # zmiana indeksowania na zgodne z tablicami (numeracja od zera)
     zakonczenie_zadan_1[arg_kolejnosc[0]] = arg_czas_na_maszynie_1[arg_kolejnosc[0]]
@@ -151,10 +146,10 @@ def przegladKolejnosci(n, arg_kolejnosc, arg_czas_na_maszynie_1, arg_czas_na_mas
     return ret_cmax
 
 
-#wczytaj konfiguracje dla dwoch maszyn
+# wczytaj konfiguracje dla dwoch maszyn
 zadania = dwie_zadania
-czas_na_maszynie_1 = dwie_czas_na_maszynie_1
-czas_na_maszynie_2 = dwie_czas_na_maszynie_2
+czas_na_maszynie_1 = dwie_czas_na_maszynie_1.copy()
+czas_na_maszynie_2 = dwie_czas_na_maszynie_2.copy()
 liczba_maszyn = dwie_liczba_maszyn
 liczba_zadan = len(zadania)
 czas = 0
@@ -210,17 +205,17 @@ najkrotsza = l1 + l2
 print("2) Algorytm Johnsona dla 2 maszyn: ", najkrotsza, "cmax=",
       przegladKolejnosci(5, najkrotsza, czas_na_maszynie_1, czas_na_maszynie_2))
 
-#wczytaj konfiguracje dla 3 maszyn
-n = trzy_zadania # zadania
-czas1 = trzy_czas_na_maszynie_1
-czas2 = trzy_czas_na_maszynie_2
-czas3 = trzy_czas_na_maszynie_3
-czasw1 = (czas1 + czas2) #laczymy czasy wykonywania sie na maszynach 1 i 2 w jeden czas
-czasw2 = (czas3 + czas2) #analogicznie dla maszyn 2 i 3
+# wczytaj konfiguracje dla 3 maszyn
+n = trzy_zadania  # zadania
+czas1 = trzy_czas_na_maszynie_1.copy()
+czas2 = trzy_czas_na_maszynie_2.copy()
+czas3 = trzy_czas_na_maszynie_3.copy()
+czasw1 = (czas1 + czas2)  # laczymy czasy wykonywania sie na maszynach 1 i 2 w jeden czas
+czasw2 = (czas3 + czas2)  # analogicznie dla maszyn 2 i 3
 # print("czasw1", czasw1, " |||| czasw2", czasw2)
 
-#w tej chwili mamy obliczone czasy dla dwoch wirtualnych maszyn, wiec mozna uzyc zwyklego algorytmu Johnsona dla dwoch maszyn
-a = list(n)  # tworzenie listy do n zadan
+# w tej chwili mamy obliczone czasy dla dwoch wirtualnych maszyn, wiec mozna uzyc zwyklego algorytmu Johnsona dla dwoch maszyn
+a = list(n)  # tworzenie listy do n zadan#
 l1 = []  # scheduler1
 l2 = []  # scheduler2
 najkrotsza = []
@@ -240,5 +235,10 @@ for k in a:
         czasw1.remove(Min1)
         czasw2.remove(Min2)
 
-najkrotsza = l1 + l2
+# najkrotsza = l1 + l2
+taa = [1, 4, 3, 2]
+najkrotsza = taa
 print("3) Algorytm Johnsona dla 3 maszyn: ", najkrotsza, "cmax=", przegladKolejnosci(4, najkrotsza, czasw1, czasw2))
+przegladKolejnosci(4, najkrotsza, trzy_czas_na_maszynie_1, trzy_czas_na_maszynie_2)
+print("czas na maszynie 1", trzy_czas_na_maszynie_1, "czas na maszynie 2", trzy_czas_na_maszynie_2, "zakonczenie zadan 1", zakonczenie_zadan_1, "zakonczenie zadan 2", zakonczenie_zadan_2)
+wizualizacjaTrzechMaszyn(trzy_czas_na_maszynie_2, najkrotsza, 'XD', 100)
