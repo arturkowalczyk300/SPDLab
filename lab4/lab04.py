79cdb54932e3010cd3969ebdf2363e2bace41a1f
Artur Kowalczyk <arturkowalczyk300@gmail.com>
2019-05-06 09:27:30 +0200

    Porzadek w kodzie

--- lab4/lab04.py
+++ lab4/lab04.py
@@ -24,18 +24,12 @@ sigma=[]
 nazwaKatalogu = "daneLab4"  # od teraz bedzie wczytywac wszystkie instancje z katalogu
 l_nazwyPlikow = []
 
-#######global #####################################
-# NOWE STRUKTURY
-###########################################
 
 m_liczbaZadan = 0
 l_czasTrwania = []  # lista zawierajaca czasy trwania na n maszynach (wiec lista dwuwymiarowa)
 l_czasZakonczenia = []
 
 wczytane = []
-
-
-# kolory = ["red", "green", "blue", "cyan", "magenta"]
 
 def permutacja(liczba):
     dlugosc = len(liczba)
@@ -78,8 +72,6 @@ def wczytajDaneZPlikuSCHRAGE(nazwaPliku):
                     r=plik_rpq[0]
                     p = plik_rpq[1]
                     q = plik_rpq[2]
-
-                   #print("r=",r," p=",p," q=",q)
                     l_zadania.append(Zadanie(r,p,q, iterator))
                 else:
                     ustawienia = [int(s) for s in linia.split() if s.isdigit()]
@@ -145,29 +137,21 @@ def Schrage():
             j = mojeMin()
             NG.append(NN[j]) #dodaje do zbioru zadan gotowych`
             del NN[j] #usuwam ze zbioru zadan nieuszeregowanych
-            #print("len(NN)=",len(NN),"len(NG)", len(NG), "suma=", len(NN)+len(NG))
-
         if(NG ==[]): #brak zadan do uporzadkowania, wiec zwiekszamy chwile czasowa
             t = NN[mojeMin()].r #najmniejsze r w zestawieniu nieuporzadkowanych
         else:
             j = mojeMax() #index
-            #sigma.append(j+tempIter)
             sigma.append(NG[j]) #dodaje zadanie do wektora kolejnosci !!!
             tempIter+=1
-            #k = k +1
             t = t + NG[j].p #dodaje czas trwania wybranego zadania
             del NG[j]
-       # cmax = max(cmax,t+ q.e)
     liczba=0
     wypelnijRozpoczeciaZadan()
     wypelnijZakonczeniaZadan()
     tempcmax=funkcjaCelu()
     for zadanie in sigma: #drukuje zadania wg kolejnosci
-        #print("id=",zadanie.id, "r=", zadanie.r, "p=",zadanie.p,"q=",zadanie.q, "rozp=", zadanie.rozpoczecie, "stop=", zadanie.zakonczenie)
         liczba+=1
     print('cmax:', tempcmax)
-    #print("#liczba zadan w wektorze kolejnosci=", liczba, "cmax=", tempcmax)
-    #print(" dl=",len(sigma),"kolejnosc", sigma, "cmax", cmax)
 
 def wypelnijRozpoczeciaZadan():
     global sigma
@@ -214,7 +198,6 @@ def Schrage_z_przerwaniami():
             j = mojeMin()
             NG.append(NN[j]) #dodaje do zbioru zadan gotowych`
              #usuwam ze zbioru zadan nieuszeregowanych
-            #print("len(NN)=",len(NN),"len(NG)", len(NG), "suma=", len(NN)+len(NG))
             if (NN[j].q > l.q):
                 l.p = t - NN[j].r
                 t = NN[j].r
